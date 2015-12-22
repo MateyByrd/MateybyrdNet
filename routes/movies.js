a5046b0df876bd53f8548cae8b95077a97933b6c
Nick Belzer <nickbelzer@gmail.com>
2015-12-22 17:14:37 +0100

    Started working on a page that displays specific information about a movie

--- routes/movies.js
+++ routes/movies.js
@@ -2,14 +2,21 @@ var express = require('express');
 var router = express.Router();
 var MovieDatabase = require('../database/movieDatabase.js');
 
+var moviedb = new MovieDatabase();
+
 router.get('/', function (req, res) {
-  var moviedb = new MovieDatabase();
 
   //moviedb.AddMovie('The Hunger Games Mockingjay part 2', 11, '20-11-2015' +
   //  ' 19:00', 'hungergames.png', 'Hunger Games', function() {});
 
   moviedb.GetAllMovies(function(docs) {
     res.render('movies', { pageId: 2, title: 'Mateybyrd.Net | Movies', image: '', movies: docs });
+  })
+});
+
+router.get('/:movieId', function(req, res) {
+  moviedb.GetMovie({"id": req.params.id}, function(docs) {
+    res.render('movieDisplay', { pageId: 2, title: "Mateybyrd.Net | " + docs.name, movie: docs });
   })
 });
 
