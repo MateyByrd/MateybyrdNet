b5f99644697c0d6bf21a303bef8abbbdd15168bd
Nick Belzer <nickbelzer@gmail.com>
2016-02-16 18:44:15 +0100

    Stats now uses execFile

--- routes/stats.js
+++ routes/stats.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var sys = require('util');
 var exec = require('child_process').exec;
+var execFile = require('child_process').execFile;
 
 router.get('/uptime', function(req, res, next) {
   exec('uptime', function(error, stdout, stderr) {
@@ -10,10 +11,10 @@ router.get('/uptime', function(req, res, next) {
 });
 
 router.get('/update', function(req, res, next) {
-  res.send('updating');
-  exec('/srv/www/updateDev.sh', function(err, stdout, stderr) {
+  execFile('node', ['/srv/www/updateDev.js'], function(err, stdout, stderr) {
     if (err) throw err;
     console.log(stdout);
+    res.send(stdout);
   });
 });
 
